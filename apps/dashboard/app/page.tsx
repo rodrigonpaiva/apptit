cd4d1a1c296473fa212d781c8e51249a2e3fe158
Deborah Malheiro <malheirodev@gmail.com>
2025-04-30 17:56:35 -0300

    feat: improve structure

--- apps/dashboard/app/page.tsx
+++ apps/dashboard/app/page.tsx
@@ -1,27 +1,7 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Oi</h1>
+      <h1 className="text-3xl font-bold underline">Apptit</h1>
     </div>
   );
 }

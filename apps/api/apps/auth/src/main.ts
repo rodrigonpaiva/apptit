b3097771aecd0c4d8ab76b453e3a6c14ea646257
Rodrigo Paiva <rodrigonpaiva@gmail.com>
2025-05-07 20:20:00 +0200

    first commit

---
+++ apps/api/apps/auth/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AuthModule } from './auth.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 8001,
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();

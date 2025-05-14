8d88dc291faaf5d283c87ca9aa59657f6d2a2703
Deborah Malheiro <malheirodev@gmail.com>
2025-05-14 17:27:33 -0300

    wip: create layout auth

--- apps/dashboard/app/(auth)/layout.tsx
+++ apps/dashboard/app/(auth)/layout.tsx
@@ -1,16 +1,16 @@
-import { ReactNode } from "react";
+import Link from "next/link";
 
-export default function AuthLayout({ children }: { children: ReactNode }  ) {
+export default function AuthLayout({ children }: { children: React.ReactNode }  ) {
     return (
-        <section className="flex flex-col justify-center items-center bg-[#F8F9FA] text-[#343A40]">
-            <div className="">
+        <section className="w-full h-screen flex flex-col justify-center items-center bg-gray-light text-gray-strong">
+            <div className="mb-16">
                 Logo
             </div>
 
-            <h1>Log in to your account</h1>
-            <h2>New here? Create an account</h2>
+            <h1 className="text-2xl font-bold mb-2">Log in to your account</h1>
+            <h2>New here? <Link href="/signup" className="text-navy-blue">Create an account</Link></h2>
 
-            <section className="">
+            <section className="mt-12">
                 {children}
             </section>
         </section>

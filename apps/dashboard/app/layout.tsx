2dd93a27dd32e04ef531444d4e1baf0f86064d9b
Deborah Malheiro <malheirodev@gmail.com>
2025-05-14 17:27:04 -0300

    feat: config font family

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -1,18 +1,21 @@
 import type { Metadata } from "next";
 import "@repo/ui/globalStyles";
+import { Sora } from 'next/font/google';
+
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Apptit",
   description: "Système intelligent de capture et traçabilité alimentaire",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={sora.variable}>
       <body className="">
         {children}
       </body>
